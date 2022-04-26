698acb4588bc8f064e2db24b12261cead19086f2
Igor Heije <igorheije85@gmail.com>
2022-04-26 15:55:12 -0300

    make it better

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.listen(4444, () => {
+app.listen(process.env.PORT || 4444, () => {
   console.log(`🚀 Listening at http:[...]:4444`);
 });
